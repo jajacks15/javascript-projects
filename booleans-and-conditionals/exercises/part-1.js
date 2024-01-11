6621cc4e01330bd78b9036e958e0cdc3b3c4b27d
Julian Jackson <jjackson2@jjackson2-23.local>
2024-01-11 01:31:23 -0500

    Booleans and Conditionals exercises

--- booleans-and-conditionals/exercises/part-1.js
+++ booleans-and-conditionals/exercises/part-1.js
@@ -1,6 +1,15 @@
 // Declare and initialize the variables for exercise 1 here:
-
+let engineIndicatorLight = "red blinking";
+let spaceSuitson = true;
+let shuttleCabinReady = true;
+let crewStatus = spaceSuitson && shuttleCabinReady;
+let computerStatusCode = 200;
+let shuttleSpeed = 15000;
 // BEFORE running the code, predict what will be printed to the console by the following statements:
+
+   /*I predict that "engines are off" will be printed in the console because 
+   the value assigned to engineIndicatorLight is not equal to "green" or "green blinking", resulting in 
+   the condition and nested conditions both being false -Julian */
 
 if (engineIndicatorLight === "green") {
    console.log("engines have started");
