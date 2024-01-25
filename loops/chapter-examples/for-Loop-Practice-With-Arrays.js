a767b59082b53666a31b2a6bf0aaa5b022b068f3
Julian Jackson <jjackson2@jjackson2-23.local>
2024-01-24 22:49:23 -0500

    Loops reading and practice

--- loops/chapter-examples/for-Loop-Practice-With-Arrays.js
+++ loops/chapter-examples/for-Loop-Practice-With-Arrays.js
@@ -1,3 +1,23 @@
 // create an array variable containing the names
+// let family = ["Julian", "Joseph", "Olivia", "Mom", "Dad"];
+// // write a for loop that prints each name on a different line
+// for (i=0; i< family.length; i++) {
+//     console.log(family[i]);
+// }
 
-// write a for loop that prints each name on a different line
+let questions = [ //question and answer arrays
+    "Who was the first American woman in space?",
+    "True or false: 5 kilometer == 5000 meters?",
+    "(5 + 3)/2 * 10 = ?",
+    "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?",
+   " What is the minimum crew size for the ISS?"
+  ];
+  let correctAnswers = [
+    "Sally Ride",
+    "true",
+    "40",
+    "Trajectory",
+    "3"
+  ];
+  let candidateAnswers = [];
+
--- loops/chapter-examples/for-Loop-Practice-With-Strings.js
+++ loops/chapter-examples/for-Loop-Practice-With-Strings.js
@@ -1,4 +1,8 @@
 // Create a string variable containing your name.
-
+let title = "Julian"
 
 // Write a for loop that prints each character in your name on a different line.
+
+for(i=0; i < title.length; i++) {
+    console.log(title[i]);
+}
