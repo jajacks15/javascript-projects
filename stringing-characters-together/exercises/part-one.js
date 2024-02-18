7011bc1152e27537f93dda918e54bed839cd0fc3
Julian Jackson <jjackson2@jjackson2-23.local>
2024-02-18 02:32:55 -0500

    Strings exercise

--- stringing-characters-together/exercises/part-one.js
+++ stringing-characters-together/exercises/part-one.js
@@ -1,4 +1,5 @@
 let num = String(1001);
+
 //Returns 'undefined'.
 
 //Use type conversion to print the length (number of digits) of an integer.
@@ -8,6 +9,7 @@ let decimal = String(123.45);
 let decimalOne = decimal.slice(0,3);
 let decimalTwo = decimal.slice(4,6);
 console.log(decimal, "has", decimalOne.length + decimalTwo.length, "digits.");
+
 //Experiment! What if num could be EITHER an integer or a decimal?  Add an if/else statement so your code can handle both cases.
 if (String(num).includes('.')){
     console.log(String(num).length-1);
