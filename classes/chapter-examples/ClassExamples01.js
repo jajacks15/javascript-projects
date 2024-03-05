f3fec2fbebeb99a8bc3f63855dfc14c58a6b1647
Julian Jackson <jjackson2@jjackson2-23.local>
2024-03-05 00:42:17 -0500

    Classes exercise work

--- classes/chapter-examples/ClassExamples01.js
+++ classes/chapter-examples/ClassExamples01.js
@@ -1,21 +1,27 @@
 //Try adding new properties inside constructor.
 class Astronaut {
-   constructor(name, age, mass){
-     this.name = name;
-     this.age = age;
-     this.mass = mass;
-   }
-}
-
+  constructor(name,age, mass, color){
+    this.name = name;
+    this.age = age;
+    this.mass = mass;
+    this.color = color;
+  };
+};
 let fox = new Astronaut('Fox', 7, 12);
+
 
 console.log(fox);
 console.log(fox.age, fox.color);
+
+fox = new Astronaut("Fox", 7, 12, "red");
 
 fox.age = 9;
 fox.color = 'red';
+fox.origin = "Africa";
 
 console.log(fox);
 console.log(fox.age, fox.color);
+console.log(fox.age, fox.color, fox.origin);
+console.log(fox);
 
 //Try modifying or adding properties below.
--- classes/chapter-examples/ClassExamples02.js
+++ classes/chapter-examples/ClassExamples02.js
@@ -3,15 +3,17 @@
 // Next, set default values for 1 or more of the parameters in constructor.
 
 class Astronaut {
-   constructor(name, age, mass){
+   constructor(name, age, mass, city = "Philly", experience = "expert"){
      this.name = name;
      this.age = age;
      this.mass = mass;
+     this.city = city;
+     this.experience = experience
    }
 }
 
-let tortoise = new Astronaut('Speedy', 120);
+let tortoise = new Astronaut('Speedy', 120, 25, "New York", "Novice");
 
-console.log(tortoise.name, tortoise.age, tortoise.mass);
+console.log(tortoise.name, tortoise.age, tortoise.mass, tortoise.city, tortoise.experience);
 
 // What happens if we call Astronaut and pass in MORE than 3 arguments? TRY IT!
